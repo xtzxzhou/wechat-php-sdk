eaf1780b2486fac26ac2b46827ebfb23ee141ecb
xtzxzhou <309187620@qq.com>
2014-12-08 21:53:38 +0800

    Update wechat.class.php

--- wechat.class.php
+++ wechat.class.php
@@ -6,9 +6,9 @@
  *  @version 1.2
  *  usage:
  *   $options = array(
- *			'token'=>'tokenaccesskey', //填写你设定的key
- *			'appid'=>'wxdk1234567890', //填写高级调用功能的app id
- *			'appsecret'=>'xxxxxxxxxxxxxxxxxxx', //填写高级调用功能的密钥
+ *			'token'=>'weixin', //填写你设定的key
+ *			'appid'=>'wxed55335793ee0a87', //填写高级调用功能的app id
+ *			'appsecret'=>'3b4aa1a55567152d44ef7247cc4208c1', //填写高级调用功能的密钥
  *		);
  *	 $weObj = new Wechat($options);
  *   $weObj->valid();
